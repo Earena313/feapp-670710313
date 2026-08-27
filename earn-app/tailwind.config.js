8ea100acf614f79cf1ec3a23b3762f634fdafc3b
Kulpreya Thongprayong <thongprayong_k@silpakorn.edu>
2026-08-27 05:31:52 +0000

    MoviesProject

---
+++ earn-app/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"], //ให้เรียกใช้เทวินได้แค่ไฟล์ที่ลงท้ายด้วย.js และ .jxs
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
