6e7064349c33237418e7fc83af271fd0fab11ff2
Kulpreya Thongprayong <thongprayong_k@silpakorn.edu>
2026-09-17 05:17:00 +0000

    week8

--- earn-app/src/pages/Home.jsx
+++ earn-app/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import MovieGallery from "../components/MovieGallery";
 function Home() {
   return (
     <div className="p-8 text-center">
-      <h1 className="mt-8 text-2xl font-bold text-slate-800">🏠 หน้าแรก</h1>
+      <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
       <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
       
       
